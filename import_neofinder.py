84e96f562a6ae154901eecf8528503d1c2605c8c
Simon Hohl <simon.hohl@dainst.de>
2022-11-22 17:00:40 +0100

    Strip leading catalog name before creating path

--- import_neofinder.py
+++ import_neofinder.py
@@ -93,7 +93,8 @@ def process_values(values):
     values["modified"] = modified_standardized
 
     values["neofinder_path"] = values["path"]
-    values["path"] = values["path"].replace(":", "/") 
+    values["path"] = values["path"].lstrip(f"{values['neofinder_catalog']}") 
+    values["path"] = values["path"].replace(":", "/")
 
     values["neofinder_type"] = values["type"]
     if values["type"] in DIRECTORY_VARIANTS:
